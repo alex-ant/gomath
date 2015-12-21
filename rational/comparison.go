0dd3113801f15407941fa9bee3c611b9371a4be8
Aleksandrs Antonovs <alex.ant0303@gmail.com>
2015-12-21 23:48:48 +0200

    added comparison methods

---
+++ rational/comparison.go
@@ -0,0 +1,21 @@
+package rational
+
+// GreaterThan returns true if a rational is greater than the passed one.
+func (ev Rational) GreaterThan(e Rational) bool {
+	return ev.numerator*e.denominator > e.numerator*ev.denominator
+}
+
+// GreaterThanNum returns true if a rational is greater than the passed integer.
+func (ev Rational) GreaterThanNum(i int64) bool {
+	return ev.GreaterThan(NewRational(i, 1))
+}
+
+// LessThan returns true if a rational is less than the passed one.
+func (ev Rational) LessThan(e Rational) bool {
+	return ev.numerator*e.denominator < e.numerator*ev.denominator
+}
+
+// LessThanNum returns true if a rational is less than the passed integer.
+func (ev Rational) LessThanNum(i int64) bool {
+	return ev.LessThan(NewRational(i, 1))
+}
