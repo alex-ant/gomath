fe77cce834383435c9ff349b76b9f19b372c226e
Aleksandrs Antonovs <alex.ant0303@gmail.com>
2015-12-21 23:15:59 +0200

    added rational numbers' operation kit

---
+++ rational/rational.go
@@ -0,0 +1,111 @@
+package rational
+
+// Rational stores a rational value.
+type Rational struct {
+	numerator   int64
+	denominator int64
+}
+
+// NewRational returns new rational number representation.
+func NewRational(n, d int64) (ev Rational) {
+	return Rational{
+		numerator:   n,
+		denominator: d,
+	}
+}
+
+// Divide divides a rational value by the provided one.
+func (ev Rational) Divide(e Rational) (nv Rational) {
+	newNumerator := ev.numerator * e.denominator
+	newDenominator := ev.denominator * e.numerator
+	if newNumerator == newDenominator {
+		nv = NewRational(1, 1)
+	} else {
+		solveNegatives(&newNumerator, &newDenominator)
+		nv = NewRational(newNumerator, newDenominator)
+		nv.Simplify()
+	}
+
+	return
+}
+
+// DivideByNum divides a rational value by the provided integer.
+func (ev Rational) DivideByNum(i int64) Rational {
+	return ev.Divide(NewRational(i, 1))
+}
+
+// Multiply multiplies a rational value by provided one.
+func (ev Rational) Multiply(e Rational) (nv Rational) {
+	newNumerator := ev.numerator * e.numerator
+	if newNumerator != 0 {
+		newDenominator := ev.denominator * e.denominator
+		if newNumerator == newDenominator {
+			nv = NewRational(1, 1)
+		} else {
+			solveNegatives(&newNumerator, &newDenominator)
+			nv = NewRational(newNumerator, newDenominator)
+			nv.Simplify()
+		}
+	}
+	return
+}
+
+// MultiplyByNum multiplies a rational value by the provided integer.
+func (ev Rational) MultiplyByNum(i int64) Rational {
+	return ev.Multiply(NewRational(i, 1))
+}
+
+// Add adds the provided rational value to an existing one.
+func (ev Rational) Add(e Rational) (nv Rational) {
+	newNumerator := ev.numerator*e.denominator + e.numerator*ev.denominator
+	if newNumerator != 0 {
+		newDenominator := ev.denominator * e.denominator
+		if newNumerator == newDenominator {
+			nv = NewRational(1, 1)
+		} else {
+			solveNegatives(&newNumerator, &newDenominator)
+			nv = NewRational(newNumerator, newDenominator)
+			nv.Simplify()
+		}
+	}
+	return
+}
+
+// AddNum adds the provided integer to an existing rational number.
+func (ev Rational) AddNum(i int64) Rational {
+	return ev.Add(NewRational(i, 1))
+}
+
+// Subtract subtracts the provided rational value from an existing one.
+func (ev Rational) Subtract(e Rational) (nv Rational) {
+	newNumerator := ev.numerator*e.denominator - e.numerator*ev.denominator
+	if newNumerator != 0 {
+		newDenominator := ev.denominator * e.denominator
+		if newNumerator == newDenominator {
+			nv = NewRational(1, 1)
+		} else {
+			solveNegatives(&newNumerator, &newDenominator)
+			nv = NewRational(newNumerator, newDenominator)
+			nv.Simplify()
+		}
+	}
+	return
+}
+
+// SubtractNum subtracts the provided integer from an existing rational number.
+func (ev Rational) SubtractNum(i int64) Rational {
+	return ev.Subtract(NewRational(i, 1))
+}
+
+// GetRationalMatrix converts int64 matrix into Rational matrix.
+func GetRationalMatrix(m [][]int64) (m2 [][]Rational) {
+	m2 = make([][]Rational, len(m))
+	for i, iv := range m {
+		mr := make([]Rational, len(m[i]))
+		for j, jv := range iv {
+			mr[j] = NewRational(jv, 1)
+		}
+		m2[i] = mr
+	}
+	return
+}
---
+++ rational/util.go
@@ -0,0 +1,62 @@
+package rational
+
+import "math/big"
+
+// Simplify simplifies the rational number by dividing it's numerator and
+// denominator by the GCD.
+func (ev *Rational) Simplify() {
+	currentNumerator := ev.numerator
+	currentDenominator := ev.denominator
+
+	if currentNumerator < 0 && currentDenominator > 0 {
+		currentNumerator *= -1
+	} else if currentDenominator < 0 && currentNumerator >= 0 {
+		currentDenominator *= -1
+	}
+
+	n := big.NewInt(currentNumerator)
+	d := big.NewInt(currentDenominator)
+
+	gcd := new(big.Int).GCD(nil, nil, n, d)
+
+	if gcd.Int64() > 1 {
+		ev.numerator /= gcd.Int64()
+		ev.denominator /= gcd.Int64()
+	}
+}
+
+// Float64 returns the float64 representation of a rational number.
+func (ev Rational) Float64() float64 {
+	return float64(ev.numerator) / float64(ev.denominator)
+}
+
+// Get returns a value.
+func (ev Rational) Get() (numerator, denominator int64) {
+	return ev.numerator, ev.denominator
+}
+
+// IsNull determines whether the value is zero.
+func (ev Rational) IsNull() (n bool) {
+	if ev.numerator == 0 && ev.denominator == 0 {
+		n = true
+	}
+	return
+}
+
+// RationalsAreNull determines whether the slice of Rationals contains only zero values.
+func RationalsAreNull(l []Rational) (isNull bool) {
+	isNull = true
+	for _, v := range l {
+		if !v.IsNull() {
+			isNull = false
+		}
+	}
+	return
+}
+
+func solveNegatives(n, d *int64) {
+	if *n < 0 && *d < 0 {
+		*n *= -1
+		*d *= -1
+	}
+}
