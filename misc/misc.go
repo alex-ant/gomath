95e4fad97f410e966729046a7c0afab0501f0405
Aleksandrs Antonovs <alex.ant0303@gmail.com>
2015-12-23 23:08:20 +0200

    calculating the GCD for multiple integers

---
+++ misc/misc.go
@@ -0,0 +1,29 @@
+package misc
+
+import "math/big"
+
+// MultiGCD returns a Greatest Common Delimiter for all the numbers in the slice.
+func MultiGCD(line []int64) (gcd int64) {
+	var uniq []int64
+	for _, v := range line {
+		var exists bool
+		for _, d := range uniq {
+			if v == d {
+				exists = true
+				break
+			}
+		}
+		if !exists {
+			uniq = append(uniq, v)
+		}
+	}
+
+	gcd = uniq[0]
+	for i := 1; i < len(uniq); i++ {
+		n := big.NewInt(gcd)
+		d := big.NewInt(uniq[i])
+		gcd = new(big.Int).GCD(nil, nil, n, d).Int64()
+	}
+
+	return
+}
