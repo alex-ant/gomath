088bff7fcd22636425c43954eed44d39bc937246
Aleksandrs Antonovs <alex.ant0303@gmail.com>
2015-12-28 13:40:47 +0200

    processing ranges asynchronously

--- gaussian-elimination/range.go
+++ gaussian-elimination/range.go
@@ -1,10 +1,22 @@
 package gaussian
 
 import (
+	"encoding/json"
 	"math"
+	"sync"
 
 	"github.com/alex-ant/gomath/rational"
 )
+
+func sliceToString(data []int64) string {
+	b, _ := json.Marshal(data)
+	return string(b)
+}
+
+func stringToSlice(data string) (sl []int64) {
+	json.Unmarshal([]byte(data), &sl)
+	return
+}
 
 // GetAllOptionsInRange retuns a list of all the possible variables' values within
 // the passed range.
@@ -16,34 +28,45 @@ func GetAllOptionsInRange(res [][]rational.Rational, min, max int64, natural boo
 		comb[i] = min
 	}
 
-	for i := 1; i <= int(math.Pow(float64(int(max-min+1)), float64(unknowns))); i++ {
-		var tmpRes []rational.Rational
-		fitsLimit := true
-		for _, jv := range res {
-			if jv[0] == rational.New(0, 0) {
-				break
-			}
-			var eqValue rational.Rational
-			for k, kv := range jv {
-				if k == 0 {
-					eqValue = kv
+	concCh := make(chan bool, 20)
+
+	var resMutex sync.Mutex
+
+	for i := int64(1); i <= int64(math.Pow(float64(int(max-min+1)), float64(unknowns))); i++ {
+		concCh <- true
+		go func(combStr string) {
+			combSl := stringToSlice(combStr)
+			var tmpRes []rational.Rational
+			fitsLimit := true
+			for _, jv := range res {
+				if jv[0] == rational.New(0, 0) {
+					break
+				}
+				var eqValue rational.Rational
+				for k, kv := range jv {
+					if k == 0 {
+						eqValue = kv
+					} else {
+						eqValue = eqValue.Subtract(kv.MultiplyByNum(combSl[k-1]))
+					}
+				}
+				if (eqValue.GreaterThanNum(max) || eqValue.LessThanNum(min)) || (natural && !eqValue.IsNatural()) {
+					fitsLimit = false
 				} else {
-					eqValue = eqValue.Subtract(kv.MultiplyByNum(comb[k-1]))
+					tmpRes = append(tmpRes, eqValue)
 				}
 			}
-			if (eqValue.GreaterThanNum(max) || eqValue.LessThanNum(min)) || (natural && !eqValue.IsNatural()) {
-				fitsLimit = false
-			} else {
-				tmpRes = append(tmpRes, eqValue)
-			}
-		}
 
-		if fitsLimit {
-			for j := len(comb) - 1; j >= 0; j-- {
-				tmpRes = append(tmpRes, rational.New(comb[j], 1))
+			if fitsLimit {
+				for j := len(combSl) - 1; j >= 0; j-- {
+					tmpRes = append(tmpRes, rational.New(combSl[j], 1))
+				}
+				resMutex.Lock()
+				res2 = append(res2, tmpRes)
+				resMutex.Unlock()
 			}
-			res2 = append(res2, tmpRes)
-		}
+			<-concCh
+		}(sliceToString(comb))
 
 		for k := unknowns - 1; k >= 0; k-- {
 			if comb[k] == max {
@@ -53,6 +76,10 @@ func GetAllOptionsInRange(res [][]rational.Rational, min, max int64, natural boo
 				break
 			}
 		}
+	}
+
+	for i := 0; i < cap(concCh); i++ {
+		concCh <- true
 	}
 
 	return
